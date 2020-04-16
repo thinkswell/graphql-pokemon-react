79f055655ea53eb01b341fcf7450b071b6e955f6
Krishna Vishwakarma <krishna23712@gmail.com>
2020-04-16 13:33:09 +0530

    Added input bar to fetch required number of Pokemon

--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -1,26 +1,38 @@
-import React from "react";
-import { useQuery } from "@apollo/react-hooks";
+import React, { useState } from "react";
 import Pokemon from "../components/Pokemon";
 import { GET_POKEMONS } from "../graphql/getPokemon";
 import { Query } from "react-apollo";
-import gql from "graphql-tag";
 
 export default function PokemonContainer() {
+  const [count, setCount] = useState(6);
+
   return (
-    <div className="container">
-      <Query query={GET_POKEMONS} variables={{ first: 10 }}>
-        {({ loading, error, data }) => {
-          if (loading) return "Loading...";
-          if (error) return `Error ${error}`;
-          return (
-            <React.Fragment>
-              {data.pokemons.map((pokemon) => (
-                <Pokemon key={pokemon.id} pokemon={pokemon} />
-              ))}
-            </React.Fragment>
-          );
-        }}
-      </Query>
-    </div>
+    <React.Fragment>
+      {" "}
+      <label>
+        Showing First{" "}
+        <input
+          type="number"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />{" "}
+        Pokemons.
+      </label>
+      <div className="container">
+        <Query query={GET_POKEMONS} variables={{ first: count }}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error ${error}`;
+            return (
+              <React.Fragment>
+                {data.pokemons.map((pokemon) => (
+                  <Pokemon key={pokemon.id} pokemon={pokemon} />
+                ))}
+              </React.Fragment>
+            );
+          }}
+        </Query>
+      </div>
+    </React.Fragment>
   );
 }
